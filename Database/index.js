885d405ffb24aba8886b296f5ba2f65cc4c40dc6
Skandan Senthil Nathan <skandansn@protonmail.com>
2023-11-18 16:53:16 -0500

    try using require for import

--- Database/index.js
+++ Database/index.js
@@ -1,9 +1,13 @@
-import courses from "./courses.json";
-import modules from "./modules.json";
-import assignments from "./assignments.json";
-import users from "./users.json";
-import grades from "./grades.json";
-import enrollments from "./enrollments.json";
+
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const courses = require("./courses.json");
+const modules = require("./modules.json");
+const assignments = require("./assignments.json");
+const users = require("./users.json");
+const grades = require("./grades.json");
+const enrollments = require("./enrollments.json");
+
 export default {
     courses,
     modules,
